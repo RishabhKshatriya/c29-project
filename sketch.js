63e4320280162fac64c6067c11fc3e3bf2a10fe8
RishabhKshatriya <68421561+RishabhKshatriya@users.noreply.github.com>
2020-08-18 20:50:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+
+function setup() {
+  
+  createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  createSprite(400, 200, 50, 50);
+
+  slingshot.display();
+
+  Engine.run(engine);
+}
+
+function draw() {
+  background(255,255,255);
+   
+
+  slingshot.display();
+
+  drawSprites();
+}
